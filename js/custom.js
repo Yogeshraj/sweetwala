640240a763adad61ac828783807852cf3e26dfe2
Yogesh <rajyogesh46@gmail.com>
2017-11-04 16:32:13 +0530

    draft2

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
 
  /* jQuery Pre loader
   -----------------------------------------------*/
-$(window).load(function(){
-    $('.preloader').fadeOut(1000); // set duration in brackets    
-});
+// $(window).load(function(){
+//     $('.preloader').fadeOut(1000); // set duration in brackets    
+// });
 
 
 /* Magnific Popup
